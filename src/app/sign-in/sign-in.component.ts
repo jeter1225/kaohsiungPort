c2a01db6d7e203f991d6a22b31f4449d4bcacc90
huangfu <nightlight870213@gmail.com>
2018-11-09 17:16:28 +0800

    fff

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ConfigLogService } from '../service-summary/config-log.service';
-import { Hero } from '../hero';
-import { PilotStatusTableItem } from '../pilot-status-table/pilot-status-table-datasource';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,13 +10,38 @@ import { PilotStatusTableItem } from '../pilot-status-table/pilot-status-table-d
 })
 export class SignInComponent implements OnInit {
 
-  config: PilotStatusTableItem[];
-
+  accountValue: string;
+  passwordValue: string;
+  confirmLog: boolean = true;
+  
   constructor(
-  	private configLogService: ConfigLogService
+    private configLogService: ConfigLogService,
+    private router: Router, 
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+  }
+
+  checkLog() {
+    if(this.accountValue == " " || this.passwordValue == " "){
+      console.error('error');
+    }
+    if(this.accountValue == "f" && this.passwordValue == "d"){
+      this.router.navigate(['homepage/information']);
+    }
+  }
+
+  forgetPassword() {
+    this.router.navigate(['fpassword']);
+  }
+
+  returnAccount(event: any) {
+    this.accountValue = event.target.value;
+  }
+
+  returnPass(event: any) {
+    this.passwordValue = event.target.value;
   }
 
 }
