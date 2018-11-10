130152340aab65635b61fe7ff89d35491db6596b
huangfu <nightlight870213@gmail.com>
2018-11-10 15:52:21 +0800

    create logout

---
+++ src/app/dialog-summary/logout/logout.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<LogoutComponent>,
+    private router: Router, 
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+  }
+
+  clickLogout(): void {
+    this.dialogRef.close();
+    this.router.navigate(['sign-in']);
+  }
+
+}
--- src/app/dialog-summary/send-password-dialog/send-password-dialog.component.ts
+++ src/app/dialog-summary/send-password-dialog/send-password-dialog.component.ts
@@ -9,7 +9,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class SendPasswordDialogComponent implements OnInit {
 
   constructor(
-    public dialogRef: MatDialogRef<SendPasswordDialogComponent>
+    private dialogRef: MatDialogRef<SendPasswordDialogComponent>
   ) { }
 
   ngOnInit() {
