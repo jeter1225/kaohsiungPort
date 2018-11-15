20c79df1c97882f79234961d9a9298a0eca70b40
jeter <b05705054@ntu.edu.tw>
2018-11-15 14:44:17 +0800

    search

--- src/app/table/information-table/information-table.component.ts
+++ src/app/table/information-table/information-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatTableDataSource} from '@angular/material';
 import { InformationService } from '../../service-summary/information-service.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-information-table',
@@ -9,7 +10,12 @@ import { InformationService } from '../../service-summary/information-service.se
 })
 export class InformationTableComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  searchKey: string;
+  searchKey1: string;
+  searchKey2: string;
+  searchKey3: string;
+  searchKey4: string;
+
+  filterJson = {vslNumber: '',vslName:'', mooringInTransferOut:'', agentCodeName:''};
   dataSource: MatTableDataSource<any>;
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['applyTime_vslSituation_serialNumber', 'vslName', 'pilot', 'vslNumber_voyage_mooringInTransferOut',
@@ -26,12 +32,44 @@ export class InformationTableComponent implements OnInit {
       this.dataSource.paginator = this.paginator;
       this.informationService.getInformation().subscribe(information_table_list => this.dataSource.data = information_table_list);
       setTimeout(() => {console.log(this.dataSource.data)}, 3000);
+      this.dataSource.filterPredicate = (data, filter: string) => {
+        const filters = JSON.parse(filter);
+        return data.vslNumber.toLowerCase().includes(filters.vslNumber) 
+               && (data.vslName_chi.includes(filters.vslName) || data.vslName_eng.toLowerCase().includes(filters.vslName)) 
+               && data.vsl_from_to.toLowerCase().includes(filters.mooringInTransferOut) 
+               && data.agentCodeName.toLowerCase().includes(filters.agentCodeName) ;
+      }
     };
-  onSearchClear(){
-    this.searchKey= "";
-    this.applyFilter();
+  onSearchClear1(){
+    this.searchKey1= "";
+    this.applyFilter1();
   };
-  applyFilter(){
-    this.dataSource.filter =this.searchKey.trim().toLowerCase();
+  applyFilter1(){
+    this.filterJson.vslNumber = this.searchKey1.trim().toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterJson);
+  };
+  onSearchClear2(){
+    this.searchKey2= "";
+    this.applyFilter2();
+  };
+  applyFilter2(){
+    this.filterJson.vslName = this.searchKey2.trim().toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterJson);
+  };
+  onSearchClear3(){
+    this.searchKey3= "";
+    this.applyFilter3();
+  };
+  applyFilter3(){
+    this.filterJson.mooringInTransferOut = this.searchKey3.trim().toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterJson);
+  };
+  onSearchClear4(){
+    this.searchKey4= "";
+    this.applyFilter4();
+  };
+  applyFilter4(){
+    this.filterJson.agentCodeName = this.searchKey4.trim().toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterJson);
   };
 }
